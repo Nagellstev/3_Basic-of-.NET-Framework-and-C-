141a952424633f8ec29af9cf059f1d745a57a010
PavelD <nagellstev@ya.ru>
2023-01-31 16:15:55 +0400

    Modified according to comments

--- DecimalToNewBaseConverter/Program.cs
+++ DecimalToNewBaseConverter/Program.cs
@@ -10,40 +10,40 @@ namespace BaseConverterProgram
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
             bool endApp = false;
             Console.WriteLine("Console base converter on C#\r");
             Console.WriteLine("-----------\n");
             while (!endApp)
             {
-                string numInput1 = "";
-                string numInput2 = "";
+                string numInput = "";
+                string baseInput = "";
                 string result = "";
 
                 Console.WriteLine("Type integer number, press Enter");
-                numInput1 = Console.ReadLine();
-                int cleanNum1 = 0;
-                while (!int.TryParse(numInput1, out cleanNum1))
+                numInput = Console.ReadLine();
+                int cleanNum = 0;
+                while (!int.TryParse(numInput, out cleanNum))
                 {
                     Console.WriteLine("Input integer number:");
-                    numInput1 = Console.ReadLine();
+                    numInput = Console.ReadLine();
                 }
 
                 Console.WriteLine("Type new base of number (0 < base < 36), press Enter");
-                numInput2 = Console.ReadLine();
-                int cleanNum2 = 0;
-                while (!int.TryParse(numInput2, out cleanNum2) ||
-                    cleanNum2 > 36 || 
-                    cleanNum2 < 2)
+                baseInput = Console.ReadLine();
+                int cleanBase = 0;
+                while (!int.TryParse(baseInput, out cleanBase) ||
+                    cleanBase > 36 || 
+                    cleanBase < 2)
                 {
                     Console.WriteLine("Type new base of number (2 <= base <= 36):");
-                    numInput2 = Console.ReadLine();
+                    baseInput = Console.ReadLine();
                 }
                 try
                 {
-                    result = ConvertBase(cleanNum1, cleanNum2);
-                    Console.WriteLine(numInput1 + " in base " + numInput2 + " is: " + result + "\n");
+                    result = ConvertBase(cleanNum, cleanBase);
+                    Console.WriteLine($"{numInput} in base {baseInput} is: {result}\n");
                 }
                 catch (Exception e)
                 {
@@ -59,6 +59,7 @@ namespace BaseConverterProgram
             }
             return;
         }
+
         public static string ConvertBase(int dec, int bas)
         {
             string output = "";
